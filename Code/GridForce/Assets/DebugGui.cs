c24529d94a4cafd9ef5b7f2e96675ea092e559d3
Markus Wall <markus_wall@web.de>
2013-11-18 14:38:51 +0100

    Code (18.11.2013 - 14:37)
    Erste Code-Version

---
+++ Code/GridForce/Assets/DebugGui.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugGui : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.gameObject.guiText.text = "";	
+	}
+}
