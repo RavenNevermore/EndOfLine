9045962ce18970bd0a1b15a2999d6d793968abb7
Markus Wall <markus_wall@web.de>
2013-11-30 13:59:09 +0100

    Code (30.11.2013, 13:58 Uhr)
    Minimale Umstrukturierung der Arena.

---
+++ GridForce/Assets/Scripts/ArenaSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArenaSettings : MonoBehaviour
+{
+    public float tileRatio = 1.0f;
+
+	// Set tiling for all children
+	void Start ()
+    {
+        MeshRenderer[] allChildren = this.GetComponentsInChildren <MeshRenderer>();
+        foreach (MeshRenderer child in allChildren)
+        {
+            child.material.mainTextureScale = new Vector2(child.gameObject.transform.localScale.x * tileRatio, child.gameObject.transform.localScale.y * tileRatio);
+        }
+	
+	}
+}
---
+++ GridForce/Assets/Scripts/DriverController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class DriverController : MonoBehaviour
+{
+    private CharacterController characterController;        // Character controller
+    private Vector3 moveDirection;          // Character move direction
+    private Vector3 gravityDirection;       // Character gravity
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.characterController = this.gameObject.GetComponent<CharacterController>();
+        this.moveDirection = this.transform.forward;
+        this.gravityDirection = -this.transform.up;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!(this.characterController.isGrounded))
+        {
+            this.characterController.Move(this.gravityDirection * Physics.gravity.magnitude * Time.deltaTime);
+        }
+
+	}
+}
