b2cadd846415c487fd9c68179c54329073d29a65
Celine Pradella <celine.pradella@web.de>
2014-01-08 17:22:22 +0100

    Main Menu die Zweite

---
+++ GridForce/Assets/RadarMenu/BtnJoinBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BtnJoinBehaviour : AbstractMenuBehaviour {
+	
+	public Camera menuCamera;
+	public string hostName;
+	public string hostIp;
+	
+	
+	void OnMouseDown(){
+		this.gameState.type = MenuState.GameType.JOIN;
+		
+		this.gameState.hostName = this.hostName;
+		
+		this.gameState.hostIp = this.hostIp;
+		
+		
+		this.switchToMenu("03_select_vehicle");
+	}
+}
---
+++ GridForce/Assets/RadarMenu/PulseOnTime.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PulseOnTime : MonoBehaviour {
+
+	public float minValue;
+	public float maxValue;
+	public float speed;
+
+	private float t;
+	private float deltaValue;
+
+	// Use this for initialization
+	void Start () {
+		this.t = 0;
+		this.deltaValue = this.maxValue - this.minValue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float delta = Time.deltaTime * this.speed;
+		this.t += delta;
+
+		float sinus = (Mathf.Sin(this.t) + 1) / 2;
+
+		this.light.intensity = this.minValue + (sinus * this.deltaValue);
+	}
+}
