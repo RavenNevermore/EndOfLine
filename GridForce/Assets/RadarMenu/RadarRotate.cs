49231fc2512a747819179c0fafaec59bd4603b59
Celine Pradella <celine.pradella@web.de>
2014-01-08 17:47:24 +0100

    main menu sky

--- GridForce/Assets/RadarMenu/RadarRotate.cs
+++ GridForce/Assets/RadarMenu/RadarRotate.cs
@@ -3,15 +3,19 @@ using System.Collections;
 
 public class RadarRotate : MonoBehaviour {
 
+	public Vector3 rotationsVector;
+	public float speed = 1;
+
 	// Use this for initialization
 	void Start () {
-	
+		if (null == this.rotationsVector)
+						this.rotationsVector = Vector3.up;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float delta = Time.deltaTime;
+        float delta = Time.deltaTime * speed;
         this.transform.Rotate(
-            Vector3.up, 135 * delta);
+            this.rotationsVector, 135 * delta);
 	}
 }
