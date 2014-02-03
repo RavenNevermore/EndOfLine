9ce153624a163cdb5edd00b593fca3eb25b7c3c1
RavenNevermore <ravens.arie@googlemail.com>
2014-02-03 02:05:38 +0100

    Added a travel script, so that item boxes can travel...

---
+++ GridForce/Assets/Scripts/Travelling.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Travelling : MonoBehaviour {
+
+	public Transform[] pathNodes;
+	public float speed;
+	public float sloppyness;
+
+	private int nextNodeIndex;
+
+	void Start () {
+		this.nextNodeIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform node = this.pathNodes[this.nextNodeIndex];
+
+		Vector3 direction = node.position - this.transform.position;
+		Vector3 travel = direction.normalized * speed * Time.deltaTime;
+
+		this.transform.Translate(travel);
+
+		float distance = Vector3.Distance(node.position, 
+		                                  this.transform.position);
+
+		//Debug.Log("--> " + distance);
+		if (this.sloppyness > distance){
+
+			this.nextNodeIndex = (this.nextNodeIndex + 1) % this.pathNodes.Length;
+		}
+	}
+}
