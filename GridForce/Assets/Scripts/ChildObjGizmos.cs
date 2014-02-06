9d7e9502ffaf0d3c9de37dd75ff3773fd46da5e5
Lumency <pradella.celine@gmail.de>
2014-02-06 16:53:34 +0100

    Final_4 Arena

---
+++ GridForce/Assets/Scripts/ChildObjGizmos.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChildObjGizmos : MonoBehaviour {
+
+    public Color color = Color.blue;
+
+    void OnDrawGizmos() {
+        Gizmos.color = this.color;
+
+        Transform[] children = this.GetComponentsInChildren<Transform>();
+        foreach (Transform child in children){
+            Gizmos.DrawSphere(child.position, 0.5f);
+        }
+        
+    }
+}
