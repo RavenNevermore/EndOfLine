43d1e246a10c4fa341cf40780971e9f219eb000b
Markus Wall <markus_wall@web.de>
2014-02-19 23:04:02 +0100

    Code (19.02.2014, 23:03 Uhr)
    Glow11-iOS-Versuche

---
+++ GridForce/Assets/Scripts/PlatformExclusiveUse.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformExclusiveUse : MonoBehaviour
+{
+    public Component exclusiveComponent = null;
+    public TargetPlatform targetPlatform = TargetPlatform.Standalone;
+
+	// Use this for initialization
+	void Start ()
+    {
+        switch (this.targetPlatform)
+        {
+            case TargetPlatform.Standalone:
+                #if !UNITY_EDITOR && !UNITY_STANDALONE
+                
+                UnityEngine.Object.Destroy(this.exclusiveComponent);
+
+                #endif
+                break;
+
+            case TargetPlatform.iOS:
+                #if !UNITY_IPHONE
+
+                UnityEngine.Object.Destroy(this.exclusiveComponent);
+
+                #endif
+                break;
+        }
+        UnityEngine.Object.Destroy(this);
+	}
+}
+
+public enum TargetPlatform
+{
+    Standalone,
+    iOS
+}
